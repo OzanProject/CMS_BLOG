d22dd390e8ed8eea8226416a6478fd2e8e1d2846
ozanproject <admin@admin.com>
2026-01-02 07:57:41 +0700

    fix: retry escaping blade directives in json-ld schema

--- resources/views/frontend/articles/partials/json-ld.blade.php
+++ resources/views/frontend/articles/partials/json-ld.blade.php
@@ -1,7 +1,7 @@
 <script type="application/ld+json">
 {
-  "@context": "https://schema.org",
-  "@type": "NewsArticle",
+  "@@context": "https://schema.org",
+  "@@type": "NewsArticle",
   "headline": "{{ addslashes($article->title) }}",
   "image": [
     "{{ asset('storage/' . $article->featured_image) }}"
@@ -9,15 +9,15 @@
   "datePublished": "{{ $article->published_at ? $article->published_at->toIso8601String() : now()->toIso8601String() }}",
   "dateModified": "{{ $article->updated_at->toIso8601String() }}",
   "author": [{
-      "@type": "Person",
+      "@@type": "Person",
       "name": "{{ $article->user->name ?? 'Admin' }}",
       "url": "{{ route('author.show', $article->user->username ?? \Illuminate\Support\Str::slug($article->user->name ?? 'admin')) }}"
     }],
   "publisher": {
-    "@type": "Organization",
+    "@@type": "Organization",
     "name": "{{ $settings['site_name'] ?? 'DeepBlog' }}",
     "logo": {
-      "@type": "ImageObject",
+      "@@type": "ImageObject",
       "url": "{{ isset($settings['site_logo']) ? asset('storage/' . $settings['site_logo']) : asset('nextpage-lite/assets/img/logo.png') }}"
     }
   }
